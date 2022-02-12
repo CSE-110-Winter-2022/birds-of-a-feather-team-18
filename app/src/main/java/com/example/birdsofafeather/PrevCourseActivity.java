5e2fa4002bbcf2c93236634278613e1eab754e09
kennyly <ksly@ucsd.edu>
2022-02-12 15:50:02 -0800

    added drop-down menu for quarter/year

--- app/src/main/java/com/example/birdsofafeather/PrevCourseActivity.java
+++ app/src/main/java/com/example/birdsofafeather/PrevCourseActivity.java
@@ -8,6 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.example.birdsofafeather.model.IPerson;
 import com.example.birdsofafeather.model.db.AppDatabase;
 import com.example.birdsofafeather.model.db.Course;
@@ -15,12 +20,13 @@ import com.example.birdsofafeather.model.db.Person;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class PrevCourseActivity extends AppCompatActivity {
     private AppDatabase db;
-    private IPerson person;
     private int personId;
     private int currCourseCount;
 
@@ -33,22 +39,26 @@ public class PrevCourseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prev_course);
         db = AppDatabase.singleton(getApplicationContext());
-        //List<? extends IPerson> persons = db.personWithCoursesDao().getAll();
 
-        //personId = db.personWithCoursesDao().maxId() + 1;
         personId = 1;
-        //Person user = new Person(personId, "user");
-        //db.personWithCoursesDao().insert(user);
-
-//        Course nCourse = new Course(db.coursesDao().maxId()+1, personId, "CSE110");
-  //      db.coursesDao().insert(nCourse);
-
         List<Course> courses = db.coursesDao().getForPerson(personId);
 
+        // Quarter Drop-down functionality
+        Spinner spinnerQuarter = findViewById(R.id.quarter_spinner);
+        ArrayAdapter<CharSequence> quarterArrAdapter = ArrayAdapter.createFromResource
+                (this,R.array.quarter, android.R.layout.simple_spinner_item);
+        quarterArrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerQuarter.setAdapter(quarterArrAdapter);
 
-
-
-        //Course[] courses = {new Course(0,0,"CSE110")};
+        //Year Drop-down functionality
+        Spinner spinnerYear = findViewById(R.id.year_spinner);
+        ArrayList<String> yearList = new ArrayList<String>();
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+        for(int y = thisYear - 10; y <= thisYear; y++){
+            yearList.add(Integer.toString(y));
+        }
+        ArrayAdapter<String> yearArrAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, yearList);
+        spinnerYear.setAdapter(yearArrAdapter);
 
 
         coursesRecyclerView = findViewById(R.id.prev_courses_view);
@@ -57,29 +67,23 @@ public class PrevCourseActivity extends AppCompatActivity {
 
         coursesViewAdapter = new CoursesViewAdapter(courses);
 
-
         coursesRecyclerView.setAdapter(coursesViewAdapter);
-
-
-
-
 
     }
 
     public void onAddPrevCourseClicked(View view){
-        //AppDatabase db = AppDatabase.singleton(getApplicationContext());
         int newCourseId = db.coursesDao().maxId()+1;
         TextView newSubjectTextView = findViewById(R.id.subject_view);
-        TextView newYearTextView = findViewById(R.id.year_view);
-        TextView newQuarterTextView = findViewById(R.id.quarter_view);
+        Spinner newYearSpinnerView = findViewById(R.id.year_spinner);
+        Spinner newQuarterSpinnerView = findViewById(R.id.quarter_spinner);
         TextView newCourseNumTextView = findViewById(R.id.course_number_view);
 
         String newSubjectText = newSubjectTextView.getText().toString();
-        String newYearText = newYearTextView.getText().toString();
-        String newQuarterText = newQuarterTextView.getText().toString();
+        String newYearText = newYearSpinnerView.getSelectedItem().toString();
+        String newQuarterText = newQuarterSpinnerView.getSelectedItem().toString();
         String newCourseNumText = newCourseNumTextView.getText().toString();
 
-        String prevCourse =  newQuarterText + newYearText + " " + newSubjectText + " " + newCourseNumText;
+        String prevCourse =  newQuarterText + " " + newYearText + " " + newSubjectText + " " + newCourseNumText;
 
         Course newCourse = new Course(newCourseId, personId, prevCourse);
 
