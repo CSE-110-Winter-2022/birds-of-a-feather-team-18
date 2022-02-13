e8b89d9e703da060a56f088cc1db8cb916b87c68
Levi <leviluo527@gmail.com>
2022-02-13 04:19:08 -0800

    set up NearBy API + messageListener to save profile

---
+++ app/src/main/java/com/example/birdsofafeather/FakedMessageListener.java
@@ -0,0 +1,71 @@
+package com.example.birdsofafeather;
+
+import com.example.birdsofafeather.model.db.AppDatabase;
+import com.example.birdsofafeather.model.db.Course;
+import com.example.birdsofafeather.model.db.Person;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class FakedMessageListener extends MessageListener {
+    private final MessageListener messageListener;
+    private final ScheduledExecutorService executor;
+
+    public FakedMessageListener(MessageListener realMessageListener, String messageStr, AppDatabase db) {
+        this.messageListener = realMessageListener;
+        this.executor = Executors.newSingleThreadScheduledExecutor();
+
+        executor.execute(() -> {
+            Message message = new Message(messageStr.getBytes(StandardCharsets.UTF_8));
+            if (messageStr == null || messageStr.length() == 0) {
+                System.out.println("No message");
+            } else {
+                this.messageListener.onFound(message);
+
+                Scanner scanner = new Scanner(messageStr);
+                //String line = "";
+                String csvSplitBy = ",";
+
+                int count = 0;
+                String name = null;
+                String photoId = null;
+                String year;
+                String quarter;
+                String courseType;
+                String courseNum;
+                String text;
+                int personId = db.personWithCoursesDao().maxId() + 1;
+                while (scanner.hasNextLine()) {
+                    String[] array = scanner.nextLine().split(csvSplitBy);
+                    if (count == 0) {
+                        name = array[0];
+                        count++;
+                    } else if (count == 1) {
+                        photoId = array[0];
+                        count++;
+
+                    } else {
+                        year = array[0];
+                        quarter = array[1];
+                        courseType = array[2];
+                        courseNum = array[3];
+                        text = quarter + year + ' ' + courseType + ' ' + courseNum;
+                        int courseId = db.coursesDao().maxId() + 1;
+                        Course c = new Course(courseId, personId, text);
+                        db.coursesDao().insert(c);
+                    }
+                }
+                Person newPerson = new Person(personId, name, photoId);
+                db.personWithCoursesDao().insert(newPerson);
+                scanner.close();
+
+                this.messageListener.onLost(message);
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeather/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +16,9 @@ import com.example.birdsofafeather.model.IPerson;
 import com.example.birdsofafeather.model.db.AppDatabase;
 import com.example.birdsofafeather.model.db.Course;
 import com.example.birdsofafeather.model.db.Person;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -24,9 +28,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-    protected RecyclerView personsRecyclerView;
-    protected RecyclerView.LayoutManager personsLayoutManager;
-    protected PersonsViewAdapter personsViewAdapter;
+    private static final String TAG = "Bofs-Nearby";
+    private MessageListener messageListener;
 
 
     @Override
@@ -34,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(R.string.app_title);
-
 
         AppDatabase db = AppDatabase.singleton(this);
 
@@ -48,6 +50,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        MessageListener realListener = new MessageListener() {};
+
+        TextView profile = findViewById(R.id.student_profile);
+        String test = profile.getText().toString();
+
+        this.messageListener = new FakedMessageListener(realListener, test, db);
+
     }
 
     public void onTestClicked(View view) {
@@ -60,94 +69,33 @@ public class MainActivity extends AppCompatActivity {
     public void onEnterClicked(View view) {
         TextView profile = findViewById(R.id.student_profile);
         String test = profile.getText().toString();
-        //String csvReader = "\\Users\\55369\\Desktop\\Study\\mock.csv";
-        /*String test = "Bill,,,\n" +
-                "https://lh3.googleusercontent.com/pw/AM-JKLXQ2ix4dg-PzLrPOSMOOy6M3PSUrijov9jCLXs4IGSTwN73B4kr-F6Nti_4KsiUU8LzDSGPSWNKnFdKIPqCQ2dFTRbARsW76pevHPBzc51nceZDZrMPmDfAYyI4XNOnPrZarGlLLUZW9wal6j-z9uA6WQ=w854-h924-no?authuser=0,,,\n" +
-                "2021,FA,CSE,210\n" +
-                "2022,WI,CSE,110\n" +
-                "2022,SP,CSE,110";*/
 
+        MessageListener realListener = new MessageListener() {
+            @Override
+            public void onFound(@NonNull Message message) {
+                Log.d(TAG, "Found Profile: " + new String(message.getContent()));
+            }
 
-        Scanner scanner = new Scanner(test);
+            @Override
+            public void onLost(@NonNull Message message) {
+                Log.d(TAG, "Profile loaded in DataBase " + new String(message.getContent()));
+            }
+        };
         AppDatabase db = AppDatabase.singleton(this);
-        //String line = "";
-        String csvSplitBy = ",";
+        //Use messageListener to save profile in database
+        this.messageListener = new FakedMessageListener(realListener, test, db);
 
-        int count = 0;
-        String name = null;
-        String photoId = null;
-        String year;
-        String quarter;
-        String courseType;
-        String courseNum;
-        String text;
-        int personId = db.personWithCoursesDao().maxId() + 1;
-        while (scanner.hasNextLine()) {
-            String[] array = scanner.nextLine().split(csvSplitBy);
-            if (count == 0) {
-                name = array[0];
-                count++;
-            } else if (count == 1) {
-                photoId = array[0];
-                count++;
+        profile.setText("");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Nearby.getMessagesClient(this).subscribe(messageListener);
+    }
 
-            } else {
-                year = array[0];
-                quarter = array[1];
-                courseType = array[2];
-                courseNum = array[3];
-                text = quarter + year + ' ' + courseType + ' ' + courseNum;
-                int courseId = db.coursesDao().maxId() + 1;
-                Course c = new Course(courseId, personId, text);
-                db.coursesDao().insert(c);
-            }
-        }
-        Person newPerson = new Person(personId, name, photoId);
-        db.personWithCoursesDao().insert(newPerson);
-        scanner.close();
-
- /*      try {
-            br = new BufferedReader(new FileReader(csvReader));
-            int count = 0;
-            String name = null;
-            String photoId = null;
-            int personId = db.personWithCoursesDao().maxId() + 1;
-            while ((line = br.readLine()) != null) {
-                String[] array = line.split(csvSplitBy);
-                if (array[1] == null && count == 0) {
-                    name = array[0];
-                    count++;
-                } else if (array[1] == null && count == 1) {
-                    photoId = array[0];
-                    count++;
-                    Person newPerson = new Person(personId, name, photoId);
-                    Log.d("Here", "Name" + name);
-                    db.personWithCoursesDao().insert(newPerson);
-                } else {
-                    String year = array[0];
-                    String quarter = array[1];
-                    String courseType = array[2];
-                    String courseNum = array[3];
-                    String text = quarter + year + ' ' + courseType + ' ' + courseNum;
-                    int courseId = db.coursesDao().maxId() + 1;
-                    Course c = new Course(courseId, personId, text);
-                    db.coursesDao().insert(c);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-*/
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Nearby.getMessagesClient(this).unsubscribe(messageListener);
     }
 }
-//FA2022 CSE 110
