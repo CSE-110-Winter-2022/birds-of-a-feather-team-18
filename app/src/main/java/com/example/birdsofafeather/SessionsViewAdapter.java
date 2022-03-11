41a94f8464379d76690a6a6ddaa4cd471de417c1
kennyly <ksly@ucsd.edu>
2022-03-10 22:12:01 -0800

    Select from current courses to name Session + No longer able to edit Favorites Session Name

--- app/src/main/java/com/example/birdsofafeather/SessionsViewAdapter.java
+++ app/src/main/java/com/example/birdsofafeather/SessionsViewAdapter.java
@@ -27,7 +27,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 
-public class SessionsViewAdapter extends RecyclerView.Adapter<SessionsViewAdapter.ViewHolder> {
+public class SessionsViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final List<Session> sessions;
     private final AppDatabase db;
 
@@ -39,21 +39,57 @@ public class SessionsViewAdapter extends RecyclerView.Adapter<SessionsViewAdapte
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
-        view = LayoutInflater
-                .from(parent.getContext())
-                .inflate(R.layout.session_row, parent, false);
-        return new ViewHolder(view, this::showRenameSessionDialog);
+        // Switch Case allows Favorites Session to not have "Session Rename Edit Button"
+        switch (viewType) {
+            // Favorites Session has viewType of 0
+            case 0:
+                // Favorites Session uses favorites_session_row layout (no edit button)
+                view = LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.favorites_session_row, parent, false);
+                return new FavoriteViewHolder(view);
+            // All other Sessions
+            default:
+                // All other Sessions uses session_row layout (has edit button)
+                view = LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.session_row, parent, false);
+                return new ViewHolder(view, this::showRenameSessionDialog);
+        }
 
     }
 
+    @Override
+    public int getItemViewType(int position){
+        // Favorites Session will have viewType 0
+        if(position == 0){
+            return 0;
+        }
+        // All other Sessions will have viewType 1
+        else{
+            return 1;
+        }
+    }
 
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.setSession(sessions.get(position));
-
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        switch(holder.getItemViewType()){
+            // Favorite Session will always be position 0
+            case 0:
+                // Favorite Session uses FavoriteViewHolder
+                FavoriteViewHolder favoriteViewHolder = (FavoriteViewHolder)holder;
+                favoriteViewHolder.setSession(sessions.get(position));
+                break;
+            // All other Sessions
+            default:
+                // All other Sessions uses ViewHolder
+                ViewHolder viewHolder = (ViewHolder)holder;
+                viewHolder.setSession(sessions.get(position));
+                break;
+        }
     }
 
     @Override
@@ -65,6 +101,7 @@ public class SessionsViewAdapter extends RecyclerView.Adapter<SessionsViewAdapte
         return sessions;
     }
 
+    // AlertDialog pop-up when renaming a session
     public void showRenameSessionDialog(Context c, int position) {
         final EditText taskEditText = new EditText(c);
         AlertDialog dialog = new AlertDialog.Builder(c)
@@ -84,16 +121,16 @@ public class SessionsViewAdapter extends RecyclerView.Adapter<SessionsViewAdapte
         dialog.show();
     }
 
-
-    public static class ViewHolder
-            extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
+    // All Sessions (except Favorites Session) ViewHolder
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final TextView sessionTextView;
         private Session session;
 
         ViewHolder(View itemView, BiConsumer<Context, Integer> showRenameSessionDialog) {
             super(itemView);
             this.sessionTextView = itemView.findViewById(R.id.session_row_name);
+
+            // "Session Rename Button" functionality
             FloatingActionButton renameFab = itemView.findViewById(R.id.rename_fab);
             itemView.setOnClickListener(this);
 
@@ -112,16 +149,31 @@ public class SessionsViewAdapter extends RecyclerView.Adapter<SessionsViewAdapte
         @Override
         public void onClick(View view) {
             Context context = view.getContext();
-            Intent intent;
-            if(session.sessionId.equals("favoritesSession")){
-                intent = new Intent(context, FavoriteListActivity.class);
-            }
-            else {
-                intent = new Intent(context, PrevPersonListActivity.class);
-                intent.putExtra("session_id", this.session.sessionId);
-            }
+            Intent intent = new Intent(context, PrevPersonListActivity.class);
+            intent.putExtra("session_id", this.session.sessionId);
             context.startActivity(intent);
         }
+    }
 
+    // Favorites Session ViewHolder
+    public static class FavoriteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private final TextView sessionTextView;
+
+        FavoriteViewHolder(View itemView) {
+            super(itemView);
+            this.sessionTextView = itemView.findViewById(R.id.session_row_name);
+            itemView.setOnClickListener(this);
+        }
+
+        public void setSession(Session session) {
+            this.sessionTextView.setText(session.sessionName);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Context context = view.getContext();
+            Intent intent = new Intent(context, FavoriteListActivity.class);
+            context.startActivity(intent);
+        }
     }
 }
